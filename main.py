c091f7356dc7434be8d11df06602ef3a77b6357f
totoro <arbc139@gmail.com>
2019-05-07 20:04:23 +0900

    Finishes to implements query generator.

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 #-*- coding: utf-8 -*-
 
+import codecs
 import datetime
+import random
+import math
 import sys
 
 from csv_manager import CsvReader
-from utils import HangulPrinter, ValueGenerator
-
-insert_sql = 'INSERT INTO {table_name}({columns}) VALUES ({values});'
+from utils import HangulPrinter, ValueGenerator, IdYearCalculator, RandomHelper, InsertSqlMaker, StringConverter
 
 def parse_commands(argv):
   from optparse import OptionParser
   parser = OptionParser('"')
   parser.add_option('', '--facultyCount', dest='faculty_count')
-  parser.add_option('', '--studentsCount', dest='student_count')
+  parser.add_option('', '--studentCount', dest='student_count')
   parser.add_option('', '--courseCount', dest='course_count')
-  parser.add_option('', '--crRatio', dest='cr_ratio') # course_registration_ratio
-  # Must make 'COURSE_TO_TIME' by 1:1 to 'COURSE'
+  parser.add_option('', '--outputFile', dest='output_file')
 
   options, otherjunk = parser.parse_args(argv)
   return options
@@ -23,76 +23,335 @@ def parse_commands(argv):
 def main():
   options = parse_commands(sys.argv[1:])
   h_printer = HangulPrinter()
-  v_generator = ValueGenerator()
 
-  faculty_count = int(options.faculty_count)
-  student_count = int(options.student_count)
-  school_ids = v_generator.school_ids(faculty_count + student_count)
-
+  # Static Tables
   # Read building.csv
-  building = None
+  buildings = None
   with open('input/building.csv', 'r') as file:
     reader = CsvReader(file)
-    building = reader.get_dict_list_data()
-  h_printer.pprint(building)
+    buildings = reader.get_dict_list_data()
+  # h_printer.pprint(buildings)
 
   # Read college.csv
-  college = None
+  colleges = None
   with open('input/college.csv', 'r') as file:
     reader = CsvReader(file)
-    college = reader.get_dict_list_data()
-  h_printer.pprint(college)
+    colleges = reader.get_dict_list_data()
+  # h_printer.pprint(colleges)
 
   # Read day_of_week.csv
-  day_of_week = None
+  day_of_weeks = None
   with open('input/day_of_week.csv', 'r') as file:
     reader = CsvReader(file)
-    day_of_week = reader.get_dict_list_data()
-  h_printer.pprint(day_of_week)
+    day_of_weeks = reader.get_dict_list_data()
+  # h_printer.pprint(day_of_weeks)
 
   # Read lectureroom.csv
-  lectureroom = None
+  lecturerooms = None
   with open('input/lectureroom.csv', 'r') as file:
     reader = CsvReader(file)
-    lectureroom = reader.get_dict_list_data()
-  h_printer.pprint(lectureroom)
+    lecturerooms = reader.get_dict_list_data()
+  # h_printer.pprint(lecturerooms)
 
   # Read semester.csv
-  semester = None
+  semesters = None
   with open('input/semester.csv', 'r') as file:
     reader = CsvReader(file)
-    semester = reader.get_dict_list_data()
-  h_printer.pprint(semester)
+    semesters = reader.get_dict_list_data()
+  # h_printer.pprint(semesters)
 
   # Read timetable.csv
-  timetable = None
+  timetables = None
   with open('input/timetable.csv', 'r') as file:
     reader = CsvReader(file)
-    timetable = reader.get_dict_list_data()
-  h_printer.pprint(timetable)
+    timetables = reader.get_dict_list_data()
+  # h_printer.pprint(timetables)
 
-  # Generate Faculty table data
-  faculty = None
-  
+  v_generator = ValueGenerator([x['MAJOR_ID'] for x in colleges])
+  id_year_calculator = IdYearCalculator()
+  r_helper = RandomHelper()
 
-  # table_name = 'test_student'
-  # name = generate_random_string(5)
-  # number = 2018111111
-  # startdate = datetime.date(1950, 3, 1)
-  # repeat = int(options.count)
+  faculty_count = len(colleges)
+  student_count = 0
+  course_count = 0
+  if options.faculty_count is not None:
+    faculty_count = int(options.faculty_count)
+  if options.student_count is not None:
+    student_count = int(options.student_count)
+  if options.course_count is not None:
+    course_count = int(options.course_count)
 
-  # with open(options.output_file, 'w+') as file:
-  #   for _ in range(repeat):
-  #     sql = insert_sql.format(**{
-  #       'table_name': table_name,
-  #       'name': "'{}'".format(name),
-  #       'number': number,
-  #       'startdate': "'{}'".format(startdate.strftime("%Y-%m-%d"))
-  #     })
-  #     file.write(sql)
-  #     name = generate_random_string(5)
-  #     number = number + 1
-  #     startdate = startdate + datetime.timedelta(days=1)
+  # Dynamic Tables
+  # Generate 'Faculty' table data
+  faculties = []
+  ids = v_generator.extract_ids(faculty_count)
+  positions = ['정교수', '부교수', '조교수']
+  # Must 1 Faculty on each College
+  for college in colleges:
+    faculty_id = ids.pop()
+    faculties.append({
+      'id': faculty_id,
+      'name': v_generator.random_string(6),
+      'major_id': college['MAJOR_ID'],
+      'position': random.choice(positions),
+    })
+  for faculty_id in ids:
+    college = random.choice(colleges)
+    faculties.append({
+      'id': faculty_id,
+      'name': v_generator.random_string(6),
+      'major_id': college['MAJOR_ID'],
+      'position': random.choice(positions),
+    })
+  # h_printer.pprint(faculties)
+  major_faculties = v_generator.get_major_faculties(faculties)
+
+  # Generate 'Students' table data
+  students = []
+  ids = v_generator.extract_ids(student_count)
+  for student_id in ids:
+    college = random.choice(colleges)
+    students.append({
+      'id': student_id,
+      'name': v_generator.random_string(6),
+      'major_id': college['MAJOR_ID'],
+      'admission_year': id_year_calculator.get_admission_year(student_id),
+      'graduate_year': id_year_calculator.get_graduate_year(student_id),
+      'grade': id_year_calculator.get_grade(student_id),
+    })
+  # h_printer.pprint(students)
+
+  # Generate 'Courses' table data
+  # -----!!----- unique key -----!!-----
+  # YEAR, SEMESTER, COURSE_ID_PREFIX, COURSE_ID_NO, DIVISION_NO
+  courses = []
+  course_unique_keys = v_generator.get_course_unique_keys(
+    count=course_count,
+    years=[2015, 2016, 2017, 2018, 2019],
+    semesters=[1, 2],
+    # All majors + 3 Generals
+    course_id_prefixes=v_generator.major_codes | set('XYZ'),
+    # course_id_prefixes=v_generator.major_codes | v_generator.get_sample_general_codes(3),
+  )
+  for i, unique_key in enumerate(course_unique_keys):
+    faculty = None
+    if unique_key['course_id_prefix'] in major_faculties:
+      faculty = random.choice(major_faculties[unique_key['course_id_prefix']])
+    else:
+      faculty = random.choice(faculties)
+    lectureroom = random.choice(lecturerooms)
+    courses.append({
+      'id': i,
+      'year': unique_key['year'],
+      'semester': unique_key['semester'],
+      'course_id_prefix': unique_key['course_id_prefix'],
+      'course_id_no': unique_key['course_id_no'],
+      'division_no': unique_key['division_no'],
+      'course_name': unique_key['name'],
+      'prof_id': faculty['id'],
+      'build_no': lectureroom['BUILDNO'],
+      'room_no': lectureroom['ROOMNO'],
+      'credit': unique_key['credit'],
+      'max_enrollees': random.randint(20, 300),
+    })
+  # h_printer.pprint(courses)
+
+  # Generate 'Course_to_time' table data
+  course_to_times = []
+  week_timetables = v_generator.combine_week_timetables(day_of_weeks, timetables)
+  # Time per credit (3 time for 3 credit)
+  for course in courses:
+    time = course['credit']
+    course_timetables = random.sample(week_timetables, k=time)
+    for ctt in course_timetables:
+      course_to_times.append({
+        'course_id': course['id'],
+        'day_of_week': ctt[0],
+        'no': ctt[1],
+      })
+  # h_printer.pprint(course_to_times)
+
+  # Generate 'Course_registration' table data
+  course_registrations = v_generator.combine_student_course(students, courses)
+  # h_printer.pprint(course_registration)
+
+  # Generate 'Attendance' table data
+  # 3 credit = 48 hours, 2 credit = 32 hours, 1 credit = 16 hours
+  attendances = []
+  course_id_map = v_generator.get_course_id_map(courses)
+  absence_probability_distribute = r_helper.non_uniform_distribute(
+    [0,   0.2, 0.4, 0.6, 0.8, 1],
+    [0.8, 0.1, 0.05, 0.02, 0.02, 0.01],
+  )
+  for cr in course_registrations:
+    total_course_time = course_id_map[cr['course_id']]['credit'] * 16
+    absence_time = int(total_course_time * random.choice(absence_probability_distribute))
+    attendances.append({
+      'course_id': cr['course_id'],
+      'student_id': cr['student_id'],
+      'absence_time': absence_time,
+    })
+
+  # Generate 'Grade' table data
+  grades = []
+  grade_map = {
+    'A+': 4.3, 'A0': 4.0, 'A-': 3.7,
+    'B+': 3.3, 'B0': 3.0, 'B-': 2.7,
+    'C+': 2.3, 'C0': 2.0, 'C-': 1.7,
+    'D+': 1.3, 'D0': 1.0, 'D-': 0.7,
+    'F': 0.0,
+  }
+  for attendance in attendances:
+    total_course_time = course_id_map[cr['course_id']]['credit'] * 16
+    absence_time = attendance['absence_time']
+    f_pivot = int(math.ceil(total_course_time / 3))
+    grade_ch = None
+    if absence_time >= f_pivot:
+      grade_ch = 'F'
+    else:
+      grade_ch = random.choice(grade_map.keys())
+    grades.append({
+      'course_id': attendance['course_id'],
+      'student_id': attendance['student_id'],
+      'grade': grade_map[grade_ch],
+    })
+
+  sql_maker = InsertSqlMaker()
+  with open(options.output_file, 'w+') as sql_file:
+    # Faculty Table
+    for faculty in faculties:
+      sql = sql_maker.make_dict(
+        table_name='FACULTY',
+        columns=['ID', 'NAME', 'MAJOR_ID', 'POSITION'],
+        value_info=[
+          { 'key': 'id', 'type': 'STRING' },
+          { 'key': 'name', 'type': 'STRING' },
+          { 'key': 'major_id', 'type': 'STRING' },
+          { 'key': 'position', 'type': 'STRING' },
+        ],
+        value_dict=faculty)
+      sql_file.write(sql)
+      sql_file.write('\n')
+
+    # Students Table
+    for student in students:
+      sql = None
+      if student['graduate_year'] is None:
+        sql = sql_maker.make_dict(
+          table_name='STUDENTS',
+          columns=[
+            'STUDENT_ID', 'NAME', 'MAJOR_ID', 'GRADE', 'ADMISSION_YEAR',
+          ],
+          value_info=[
+            { 'key': 'id', 'type': 'STRING' },
+            { 'key': 'name', 'type': 'STRING' },
+            { 'key': 'major_id', 'type': 'STRING' },
+            { 'key': 'grade', 'type': 'NUMBER' },
+            { 'key': 'admission_year', 'type': 'NUMBER' },
+          ],
+          value_dict=student)
+      else:
+        sql = sql_maker.make_dict(
+          table_name='STUDENTS',
+          columns=[
+            'STUDENT_ID', 'NAME', 'MAJOR_ID', 'GRADE', 'ADMISSION_YEAR',
+            'GRADUATE_YEAR',
+          ],
+          value_info=[
+            { 'key': 'id', 'type': 'STRING' },
+            { 'key': 'name', 'type': 'STRING' },
+            { 'key': 'major_id', 'type': 'STRING' },
+            { 'key': 'grade', 'type': 'NUMBER' },
+            { 'key': 'admission_year', 'type': 'NUMBER' },
+            { 'key': 'graduate_year', 'type': 'NUMBER' },
+          ],
+          value_dict=student)
+      sql_file.write(sql)
+      sql_file.write('\n')
+
+    # Courses Table
+    for course in courses:
+      sql = sql_maker.make_dict(
+        table_name='COURSES',
+        columns=[
+          'COURSE_ID', 'YEAR', 'SEMESTER', 'COURSE_ID_PREFIX', 'COURSE_ID_NO',
+          'DIVISION_NO', 'COURSE_NAME', 'PROF_ID', 'BUILDNO', 'ROOMNO',
+          'CREDIT', 'MAX_ENROLLEES',
+        ],
+        value_info=[
+          { 'key': 'id', 'type': 'NUMBER' },
+          { 'key': 'year', 'type': 'NUMBER' },
+          { 'key': 'semester', 'type': 'NUMBER' },
+          { 'key': 'course_id_prefix', 'type': 'STRING' },
+          { 'key': 'course_id_no', 'type': 'STRING' },
+          { 'key': 'division_no', 'type': 'STRING' },
+          { 'key': 'course_name', 'type': 'STRING' },
+          { 'key': 'prof_id', 'type': 'STRING' },
+          { 'key': 'build_no', 'type': 'STRING' },
+          { 'key': 'room_no', 'type': 'STRING' },
+          { 'key': 'credit', 'type': 'NUMBER' },
+          { 'key': 'max_enrollees', 'type': 'NUMBER' },
+        ],
+        value_dict=course)
+      sql_file.write(sql)
+      sql_file.write('\n')
+
+    # Courses_to_time Table
+    for ctt in course_to_times:
+      sql = sql_maker.make_dict(
+        table_name='COURSE_TO_TIME',
+        columns=[
+          'COURSE_ID', 'DAY_OF_WEEK', 'NO',
+        ],
+        value_info=[
+          { 'key': 'course_id', 'type': 'NUMBER' },
+          { 'key': 'day_of_week', 'type': 'STRING' },
+          { 'key': 'no', 'type': 'NUMBER' },
+        ],
+        value_dict=ctt)
+      sql_file.write(sql)
+      sql_file.write('\n')
+
+    # Course_registrations Table
+    for cr in course_registrations:
+      sql = sql_maker.make_dict(
+        table_name='COURSE_REGISTRATION',
+        columns=['COURSE_ID', 'STUDENT_ID'],
+        value_info=[
+          { 'key': 'course_id', 'type': 'NUMBER' },
+          { 'key': 'student_id', 'type': 'STRING' },
+        ],
+        value_dict=cr)
+      sql_file.write(sql)
+      sql_file.write('\n')
+
+    # Attendance Table
+    for attendance in attendances:
+      sql = sql_maker.make_dict(
+        table_name='ATTENDANCE',
+        columns=['COURSE_ID', 'STUDENT_ID', 'ABSENCE_TIME'],
+        value_info=[
+          { 'key': 'course_id', 'type': 'NUMBER' },
+          { 'key': 'student_id', 'type': 'STRING' },
+          { 'key': 'absence_time', 'type': 'NUMBER' },
+        ],
+        value_dict=attendance)
+      sql_file.write(sql)
+      sql_file.write('\n')
+
+    # Grade Table
+    for grade in grades:
+      sql = sql_maker.make_dict(
+        table_name='GRADE',
+        columns=['COURSE_ID', 'STUDENT_ID', 'GRADE'],
+        value_info=[
+          { 'key': 'course_id', 'type': 'NUMBER' },
+          { 'key': 'student_id', 'type': 'STRING' },
+          { 'key': 'grade', 'type': 'NUMBER' },
+        ],
+        value_dict=grade)
+      sql_file.write(sql)
+      sql_file.write('\n')
 
 if __name__ == '__main__':
   main()
--- utils.py
+++ utils.py
@@ -1,22 +1,239 @@
 
 import pprint
+import itertools
 import random
 import string
 
+class StringConverter():
+  def sm_stringfy(self, value):
+    return "'{}'".format(value)
+
+  def bg_stringfy(self, value):
+    return '"{}"'.format(value)
+
+class InsertSqlMaker():
+  def __init__(self):
+    self.str_converter = StringConverter()
+    self.sql_form = 'INSERT INTO {table_name}({columns}) VALUES ({values});'
+
+  def make(self, table_name, columns, values):
+    return self.sql_form.format(**{
+      'table_name': self.str_converter.bg_stringfy(table_name),
+      'columns': ','.join(
+        self.str_converter.bg_stringfy(str(column)) for column in columns),
+      'values': ','.join(str(value) for value in values),
+    })
+
+  def make_dict(self, table_name, columns, value_info, value_dict):
+    values = []
+    for info in value_info:
+      if info['type'] == 'STRING':
+        values.append(self.str_converter.sm_stringfy(value_dict[info['key']]))
+      elif info['type'] == 'NUMBER':
+        values.append(value_dict[info['key']])
+    return self.make(table_name, columns, values)
+
+class RandomHelper():
+  def non_uniform_distribute(self, values, distributes):
+    results = []
+    for i in range(len(values)):
+      value = values[i]
+      distribute = int(distributes[i] * 100)
+      results.extend([value] * distribute)
+    return results
+
+class IdYearCalculator():
+  def __init__(self):
+    self.today_year = 2019
+
+  def get_year(self, target_id):
+    return target_id / 1000000
+
+  def get_admission_year(self, target_id):
+    year = self.get_year(target_id)
+    return year
+
+  # Fast-graduate assumption
+  def get_grade(self, target_id):
+    year = self.get_year(target_id)
+    raw_grade = self.today_year - year + 1
+    if raw_grade > 5:
+      return 5
+    return raw_grade
+
+  def get_graduate_year(self, target_id):
+    grade = self.get_grade(target_id)
+    if grade < 5:
+      return None
+    year = self.get_year(target_id)
+
+    return year + 5
+
 class ValueGenerator():
+  def __init__(self, major_codes):
+    self.sample_ids = range(2015000000, 2020000000)
+    self.major_codes = set(major_codes)
+    total_codes = set([''.join(i) for i in itertools.product(string.uppercase, repeat=3)])
+    self.general_codes = total_codes - self.major_codes
+
   def random_string(self, length):
     return ''.join(random.choice(string.lowercase) for x in range(length))
 
-  def school_ids(self, count):
+  def random_digits(self, length):
+    return ''.join(random.choice(string.digits) for x in range(length))
 
-  # 2019xxxxxx
-  def school_id(self):
-    years = ['2015', '2016', '2017', '2018', '2019']
-    school_id_form = '{year}{code}'
-    return school_id_form.format(**{
-      'year': random.choice(years),
-      'code': 
-    })
+  def extract_ids(self, count):
+    ids = [
+      self.sample_ids.pop(random.randrange(len(self.sample_ids))) for _ in xrange(count)
+    ]
+    return ids
+
+  def get_sample_general_codes(self, count):
+    return set(random.sample(self.general_codes, k=count))
+
+  def get_random_courses(self, count, course_id_prefixes):
+    tuple_set = set()
+    for _ in range(count):
+      tuple_set.add((random.choice(course_id_prefixes), self.random_digits(4)))
+    results = []
+    for entry in tuple_set:
+      results.append({
+        'course_id_prefix': entry[0],
+        'course_id_no': entry[1],
+        'credit': random.randint(1, 3),
+        'name': self.random_string(8),
+      })
+    return results
+
+  # Course types
+  # 0: only 1 course (2019)
+  # 1: 2 divisions on 2019
+  # 2: 2 divisions on 2 semesters
+  # 3: 4 divisions on 2018~2019
+  # 4: 4 divisions on 2015~2019
+  def add_course_to_results(self, results, years, semesters, ctype, course):
+    if ctype == 0:
+      results.append({
+        'year': years[-1],
+        'semester': semesters[-1],
+        'course_id_prefix': course['course_id_prefix'],
+        'course_id_no': course['course_id_no'],
+        'division_no': 1,
+        'credit': course['credit'],
+        'name': course['name'],
+      })
+      return
+    elif ctype == 1:
+      for division in range(1, 3):
+        results.append({
+          'year': years[-1],
+          'semester': semesters[-1],
+          'course_id_prefix': course['course_id_prefix'],
+          'course_id_no': course['course_id_no'],
+          'division_no': division,
+          'credit': course['credit'],
+          'name': course['name'],
+        })
+      return
+    elif ctype == 2:
+      for semester in semesters:
+        for division in range(1, 3):
+          results.append({
+            'year': years[-1],
+            'semester': semester,
+            'course_id_prefix': course['course_id_prefix'],
+            'course_id_no': course['course_id_no'],
+            'division_no': division,
+            'credit': course['credit'],
+            'name': course['name'],
+          })
+      return
+    elif ctype == 3:
+      for year in years[-2:]:
+        for semester in semesters:
+          for division in range(1, 5):
+            results.append({
+              'year': year,
+              'semester': semester,
+              'course_id_prefix': course['course_id_prefix'],
+              'course_id_no': course['course_id_no'],
+              'division_no': division,
+              'credit': course['credit'],
+              'name': course['name'],
+            })
+      return
+    elif ctype == 4:
+      for year in years:
+        for semester in semesters:
+          for division in range(1, 5):
+            results.append({
+              'year': year,
+              'semester': semester,
+              'course_id_prefix': course['course_id_prefix'],
+              'course_id_no': course['course_id_no'],
+              'division_no': division,
+              'credit': course['credit'],
+              'name': course['name'],
+            })
+      return
+
+  def get_course_unique_keys(self, count, years, semesters, course_id_prefixes):
+    random_courses = self.get_random_courses(count, list(course_id_prefixes))
+    results = []
+    weighted_ctypes = [0] * 3 + [1] * 3 + [2] * 2 + [3] * 1 + [4] * 1
+    for course in random_courses:
+      ctype = random.choice(weighted_ctypes) # Randomly choosed
+      self.add_course_to_results(results, years, semesters, ctype, course)
+    return results
+
+  def get_major_faculties(self, faculties):
+    major_faculties = {}
+    for k, g in itertools.groupby(faculties, key=lambda entry: entry['major_id']):
+      if k not in major_faculties:
+        major_faculties[k] = []
+      major_faculties[k].extend(list(g))
+    return major_faculties
+
+  def get_course_id_map(self, courses):
+    course_id_map = {}
+    for k, g in itertools.groupby(courses, key=lambda entry: entry['id']):
+      group = list(g)[0]
+      course_id_map[k] = group
+    return course_id_map
+
+  def combine_week_timetables(self, day_of_weeks, timetables):
+    day_of_weeks_set = set([x['DAY_OF_WEEK'] for x in day_of_weeks])
+    timetables_set = set([x['NO'] for x in timetables])
+
+    return list(itertools.product(day_of_weeks_set, timetables_set))
+
+  def combine_student_course(self, students, courses):
+    student_set = set([x['id'] for x in students])
+    course_set = set([x['id'] for x in courses])
+
+    course_id_enrollee_counter_map = {}
+    for k, g in itertools.groupby(courses, key=lambda entry: entry['id']):
+      group = list(g)[0]
+      course_id_enrollee_counter_map[k] = {
+        'count': 0,
+        'max_enrollees': int(group['max_enrollees'] * random.uniform(0.3, 1)),
+      }
+
+    student_course_set = list(itertools.product(student_set, course_set))
+    random.shuffle(student_course_set)
+
+    results = []
+    for student_id, course_id in student_course_set:
+      enrollee_counter = course_id_enrollee_counter_map[course_id]
+
+      if (enrollee_counter['count'] >= enrollee_counter['max_enrollees']):
+        continue
+      course_id_enrollee_counter_map[course_id]['count'] = enrollee_counter['count'] + 1
+      results.append({
+        'student_id': student_id,
+        'course_id': course_id,
+      })
+    return results
 
 class HangulPrinter(pprint.PrettyPrinter):
 	def format(self, _object, context, maxlevels, level):
@@ -26,4 +243,3 @@ class HangulPrinter(pprint.PrettyPrinter):
 			_object = unicode(_object,'utf8')
 			return "'%s'" % _object.encode('utf8'), True, False
 		return pprint.PrettyPrinter.format(self, _object, context, maxlevels, level)
-
