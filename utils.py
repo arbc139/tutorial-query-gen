2ed3952fde2ee92732a13ec0a9106c9db9da06ed
Totorody <arbc139@gmail.com>
2019-05-09 19:45:56 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -236,11 +236,12 @@ class ValueGenerator():
       })
     return results
 
+# http://egloos.zum.com/mcchae/v/11076302
 class HangulPrinter(pprint.PrettyPrinter):
-	def format(self, _object, context, maxlevels, level):
-		if isinstance(_object, unicode):
-			return "'%s'" % _object.encode('utf8'), True, False
-		elif isinstance(_object, str):
-			_object = unicode(_object,'utf8')
-			return "'%s'" % _object.encode('utf8'), True, False
-		return pprint.PrettyPrinter.format(self, _object, context, maxlevels, level)
+  def format(self, _object, context, maxlevels, level):
+    if isinstance(_object, unicode):
+      return "'%s'" % _object.encode('utf8'), True, False
+    elif isinstance(_object, str):
+      _object = unicode(_object,'utf8')
+      return "'%s'" % _object.encode('utf8'), True, False
+    return pprint.PrettyPrinter.format(self, _object, context, maxlevels, level)
